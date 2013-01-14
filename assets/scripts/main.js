5d4090e6d612b8a071f54a9b66683175b62eec91
Antonin <mr.meuble@gmail.com>
2013-01-14 22:03:56 +0100

    reorganizing things a bit

--- assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-require(
-	['assets/scripts/jquery/jquery-1.8.3.js',
-	 'assets/scripts/underscore/underscore.js',
-	 'assets/scripts/backbone/backbone.js',
-	 'assets/scripts/handlebars/handlebars-1.0.rc.1.js'],
-	function() {
-		console.log('dependencies loaded');
-	}
-);
