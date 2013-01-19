f7fd910f7478c5c818921423044b440d34486f3e
Antonin <mr.meuble@gmail.com>
2013-01-19 17:05:45 +0100

    .gitignore is now working

--- app/build/build.sh
+++ app/build/build.sh
@@ -1,2 +1,17 @@
+#start
+echo "# starting build"
+
+# getting dependencies
+echo "# bower is getting dependencies"
+bower install
+
+# compiling sources
+echo "# node is compiling"
 node r.js -o app.build.js
+
+# cleaning build
+echo "# cleaning up"
 rm -rf ../../dist/build
+
+# end
+echo "# build done"
